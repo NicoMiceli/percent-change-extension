c22b5610b624e32aa760aa4508462a122a092674
Nico Miceli <NicoMiceli@gmail.com>
2014-02-07 22:08:46 -0500

    clean up js

--- percentchange.js
+++ percentchange.js
@@ -11,8 +11,6 @@ $("#calculate3").click(function () {
     processInfo3();
 })
 
-//alert('here');
-
 var id = function (id) {
     return document.getElementById(id);
 };
@@ -23,55 +21,27 @@ var cleanNumber = function(inputid){
     return cleanValue
 }
 
-var myChange = [];
-
 function processInfo() {
     var y1 = cleanNumber('old1a');
     var y2 = cleanNumber('new1b');
-    //var sum = (y2 - y1)/y1 * 100;
-    //var divide = sum / y1 * 100;
     var round = Math.round(((y2 - y1)/y1 * 100) * 100) / 100
     var myTotal = round + "%";
     var myAnswer = id('message');
     myAnswer.innerHTML = myTotal;
-    /*if (myTotal < 0) {
-        id('message').style.color="green";
-        id('message').style.fontWeight="bold";
-    }else if (myTotal === 0){
-        id('message').style.color="black"
-        id('message').style.fontWeight="bold";        
-    } else {
-        id('message').style.color="red"
-        id('message').style.fontWeight="bold";        
-    }
-    */
-    //myChange.push(myTotal);
-    //myAnswer.innerHTML += myChange.join("<br>");
-
 };
 
 function processInfo2() {
     var y1 = cleanNumber('percent2a');
     var y2 = cleanNumber('num2b');
-    //var percentage = (y1/100) * y2;
     var myTotal = Math.round(((y1/100) * y2) * 100) / 100
     var myAnswer = id('message2');
     myAnswer.innerHTML = myTotal;
-    //myChange.push(myTotal);
-    //myAnswer.innerHTML += myChange.join("<br>");
 };
-
-//(function () {
- //   id("calculate2").onclick = processInfo2;
-//})();
 
 function processInfo3() {
     var y1 = cleanNumber('num3a');
     var y2 = cleanNumber('percent3b');
-    //var percentage = y1/(y2/100);
     var myTotal = Math.round((y1/(y2/100)) * 100) / 100
     var myAnswer = id('message3');
     myAnswer.innerHTML = myTotal;
-    //myChange.push(myTotal);
-    //myAnswer.innerHTML += myChange.join("<br>");
 };
